6d19270bb7503fb37b0b3bb71ed96cba94d0275a
Fabrice Afanyu <fabricesec@outlook.com>
2022-05-17 19:20:38 -0700

    My first git commit with a script from our first tuesday meeting.

---
+++ team28g30.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is a demo script for team 28 group 30 git practice
+
+<<collins
+Hello this is a demo script for group 30. We started with a single line comment denoted with number sign #. now using the the multi line comment to pracice
+collins
+echo "Enter 1st person's name"
+read name
+echo "The first person's name is $name"
+echo "The first person's pin"
+read -s pin
+echo "Your pin is valid"
+echo " This are the individuals that were in our study group"
+echo "Fabrice, Cosma, Jumoke, Jennifer, Collins, Chidinma, & Dapo"
