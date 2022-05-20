c5016e5b7e850ad379f19079d222a2b0dfc66b55
Fabrice Afanyu <105760173+Fabops@users.noreply.github.com>
2022-05-19 17:04:22 -0700

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+Testing to create file remotely on git
+To learn to fetch new files to our local environment
+Learning new commands
+We learn the git pull command
+Using this to learn git fetch to fetch the changes in our remote repo.
